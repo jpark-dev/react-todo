def9a6925e2ca27930db09209136bb888aba0f6f
Jason Park <zeipar@gmail.com>
2022-10-09 01:11:16 -0700

    Installed minimal libraries and linted.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
+`;
+
 function App() {
-  return null;
+  return (
+    <>
+      <GlobalStyle />
+    </>
+  );
 }
 
 export default App;
