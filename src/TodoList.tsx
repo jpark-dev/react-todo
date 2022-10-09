f558e563299e5ada8190ee2ad5b7232ea6ad2c8e
Jason Park <zeipar@gmail.com>
2022-10-09 01:36:32 -0700

    Installed react-hook-form and used register/handleSubmit and formState.

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,21 +1,57 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+
+// function ToDoList() {
+//   const [toDo, setToDo] = useState("");
+//   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+//     const {
+//       currentTarget: { value },
+//     } = event;
+//     setToDo(value);
+//   };
+//   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+//     event.preventDefault();
+//     console.log(toDo);
+//   };
+//   return (
+//     <div>
+//       <form onSubmit={onSubmit}>
+//         <input onChange={onChange} placeholder="Create a ToDo" />
+//         <button>Add</button>
+//       </form>
+//     </div>
+//   );
+// }
 
 function ToDoList() {
-  const [toDo, setToDo] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setToDo(value);
+  const { register, handleSubmit, formState } = useForm();
+  const onValid = (data: any) => {
+    console.log("data", data);
   };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(toDo);
-  };
+
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} placeholder="Create a ToDo" />
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid)}
+      >
+        <input
+          {...register("First Name", { required: true })}
+          placeholder="First Name"
+        />
+        <input
+          {...register("Last Name", { required: true })}
+          placeholder="Last Name"
+        />
+        <input {...register("Email", { required: true })} placeholder="Email" />
+        <input
+          {...register("Password", { required: true, minLength: 10 })}
+          placeholder="Password"
+        />
+        <input
+          {...register("height", { required: "height too short" })}
+          placeholder="height"
+        />
         <button>Add</button>
       </form>
     </div>
