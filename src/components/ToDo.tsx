715bb9d9aab064647b7ceec9e421047b8ef49bcc
Jason Park <zeipar@gmail.com>
2022-10-11 19:47:16 -0700

    Created a logic to update the state of ToDos with simple buttons.

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,12 +1,42 @@
-import { ToDos } from "./atoms";
+import React from "react";
+import { useSetRecoilState } from "recoil";
+import { ToDos, toDoState } from "./atoms";
 
-function ToDo({ text }: ToDos) {
+function ToDo({ text, category, id }: ToDos) {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = e;
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      const newToDo = { text, id, category: name as any };
+      return [
+        ...oldToDos.slice(0, targetIndex),
+        newToDo,
+        ...oldToDos.slice(targetIndex + 1),
+      ];
+    });
+  };
+
   return (
     <li>
       <span>{text}</span>
-      <button>TO DO</button>
-      <button>IN PROGRESS</button>
-      <button>DONE</button>
+      {category !== "TO_DO" && (
+        <button name="TO_DO" onClick={onClick}>
+          TO DO
+        </button>
+      )}
+      {category !== "IN_PROGRESS" && (
+        <button name="IN_PROGRESS" onClick={onClick}>
+          IN PROGRESS
+        </button>
+      )}
+      {category !== "DONE" && (
+        <button name="DONE" onClick={onClick}>
+          DONE
+        </button>
+      )}
     </li>
   );
 }
